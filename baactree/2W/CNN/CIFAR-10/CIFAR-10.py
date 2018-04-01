74f5f2c6bb0fe3e10f0fba73001cf6de4152fc46
baactree <bsj0206@naver.com>
2018-04-02 04:27:53 +0900

    CIFAR-10 using cnn

--- baactree/2W/CNN/CIFAR-10/CIFAR-10.py
+++ baactree/2W/CNN/CIFAR-10/CIFAR-10.py
@@ -1,34 +1,32 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import scipy.misc as misc
 import random
-import main
-
-def make_data(num):
-    xd=[]
-    yd=[]
-    for size in range(num):
-        main.init()
-        number=random.randrange(0,10000)
-        main.run(number)
-        xd.append(main.photo)
-        yd.append(number)
-    xd=np.array(xd)
-    yd=np.array(yd)
-    xd=np.reshape(xd,[-1,16,64,1])
-    yd=np.reshape(yd,[-1,1])
-    return xd,yd
-
-save_file="saved/model.ckpt"
+from tensorflow.python.keras._impl.keras.datasets.cifar10 import load_data
 
 ####
+#output
+fout=open("output.txt","w")
+#
 learning_rate=0.001
-print("learning_rate : ",learning_rate)
+training_epochs=2000
+batch_size=100
+fout.write("learning_rate : %d\n"%learning_rate)
+fout.write("training_epochs : %d\n"%training_epochs)
+fout.write("batch_size : %d\n"%batch_size)
 ####
 
+label_name = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-X=tf.placeholder(tf.float32,[None,16,64,1])
+(x_train, y_train), (x_test, y_test) = load_data()
+
+
+X=tf.placeholder(tf.float32,[None,32,32,3])
 Y=tf.placeholder(tf.int32,[None,1])
-_Y=tf.reshape(tf.one_hot(Y,10000),[-1,10000])
+_Y=tf.reshape(tf.one_hot(Y,10),[-1,10])
 training=tf.placeholder(tf.bool)
 
 # convolutional layer #1
@@ -47,12 +45,12 @@ pool3=tf.layers.max_pooling2d(inputs=conv3,pool_size=[2,2],padding="same",stride
 dropout3=tf.layers.dropout(inputs=pool3,rate=0.3,training=training)
 
 # dense layer with relu
-flat=tf.reshape(dropout3,[-1,2*8*128])
+flat=tf.reshape(dropout3,[-1,4*4*128])
 dense4=tf.layers.dense(inputs=flat,units=625,activation=tf.nn.relu,kernel_initializer=tf.contrib.layers.xavier_initializer())
 dropout4=tf.layers.dropout(inputs=dense4,rate=0.5,training=training)
 
 # logit 
-logits=tf.layers.dense(inputs=dropout4,units=10000,kernel_initializer=tf.contrib.layers.xavier_initializer())
+logits=tf.layers.dense(inputs=dropout4,units=10,kernel_initializer=tf.contrib.layers.xavier_initializer())
 
 cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels=_Y))
 optimizer=tf.train.AdamOptimizer(learning_rate).minimize(cost)
@@ -60,22 +58,20 @@ optimizer=tf.train.AdamOptimizer(learning_rate).minimize(cost)
 is_correct=tf.equal(tf.argmax(logits,1),tf.argmax(_Y,1))
 accuracy=tf.reduce_mean(tf.cast(is_correct,"float"))
 
-saver=tf.train.Saver()
 
-##
-training_epochs=5
+
+
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    #saver.restore(sess,save_file)
     for epoch in range(training_epochs):
         avg_cost=0
-        total_batch=1000
-        for batch in range(total_batch):
-            batch_x,batch_y=make_data(500)
+        total_batch=int(len(x_train)/batch_size)
+        for batch_x, batch_y in zip(np.split(x_train, total_batch), np.split(y_train, total_batch)):
             batch_cost,_=sess.run([cost,optimizer],feed_dict={X:batch_x,Y:batch_y,training:True})
             avg_cost+=batch_cost/total_batch
-        print("Epoch :","%04d"%(epoch+1),"cost =","{:.9f}".format(avg_cost))
-    x_test,y_test=make_data(10000)
-    print("Final Accuracy: ",sess.run(accuracy,feed_dict={X:x_test,Y:y_test,training:False}))
-    #saver.save(sess,save_file)
+        fout.write("Epoch : %04d cost = %.9f\n"%(epoch+1,avg_cost))
+
+    fout.write("Final Accuracy: %.9f\n"%sess.run(accuracy,feed_dict={X:x_test,Y:y_test,training:False}))
+
+fout.close()
